b85e22f01ad76ac6e5c87a80eaf82f39bc754188
Pedro Sodré <pedrobsodre@outlook.com>
2026-09-11 19:43:12 -0300

    Delete front-aluno directory

--- front-aluno/front-aluno/src/hooks/useTituloDaPagina.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-/** Reflete o filtro ativo no título da aba: "Cálculo I | ClassPath". */
-export function useTituloDaPagina(sufixo) {
-  useEffect(() => {
-    document.title = sufixo ? `${sufixo} | ClassPath` : "ClassPath | Portal do aluno";
-  }, [sufixo]);
-}
